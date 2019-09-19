c5891194ab07e47e3fd9e241f18c8d19a225dd5e
Brice Chapuis <brice.chapuis@gmail.com>
2019-09-19 17:11:44 +0200

    Content Module bug with actions references fixed

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,4 +1,6 @@
 class Action < ApplicationRecord
   belongs_to :intelligence
+  has_many :content_module, dependent: :nullify, class_name: "ContentModule", foreign_key: 'action1_id', foreign_key: 'action2_id'
+  has_many :workshop_module, dependent: :nullify, class_name: "ContentModule", foreign_key: 'action1_id', foreign_key: 'action2_id'
   validates :description, length: {maximum: 140}
 end
