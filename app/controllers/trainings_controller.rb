6c42d9c8d20174a86a55f4f87f14d974c33d4ffb
Brice Chapuis <brice.chapuis@gmail.com>
2020-05-11 17:19:08 +0200

    Training create fix

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -62,7 +62,7 @@ class TrainingsController < ApplicationController
     authorize @training
     @training.refid = "#{Time.current.strftime('%y')}-#{'%04d' % (Training.where(created_at: Time.current.beginning_of_year..Time.current.end_of_year).count + 1)}"
     @training.satisfaction_survey = 'shorturl.at/gqwCZ'
-    if @training.save && @training_ownership.save
+    if @training.save
       Session.new(title: 'Session 1', date: @training.created_at, duration: 0, training_id: @training.id)
       redirect_to training_path(@training)
     else
