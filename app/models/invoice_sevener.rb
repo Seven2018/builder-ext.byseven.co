e053b769b4054cfcaf5b5072a0952208f57f2617
Brice Chapuis <brice.chapuis@gmail.com>
2020-06-25 12:18:23 +0200

    InvoiceSevenerAirtable conflict fix

--- app/models/invoice_sevener.rb
+++ app/models/invoice_sevener.rb
@@ -1,6 +1,6 @@
 Airrecord.api_key = Rails.application.credentials.airtable_key
 
-class InvoiceSevener < Airrecord::Table
+class InvoiceSevenerAirtable < Airrecord::Table
   self.base_key = 'appDqvNoJM7O4nFdn'
   self.table_name = 'Invoices'
 end
