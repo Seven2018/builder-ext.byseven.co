def8773bba377e8affde4c234767d9f40412d4e0
Brice Chapuis <brice.chapuis@gmail.com>
2020-07-06 10:39:21 +0200

    Airtable models added

--- app/models/overview_invoice_sevener.rb
+++ app/models/overview_invoice_sevener.rb
@@ -1,6 +1,6 @@
 Airrecord.api_key = Rails.application.credentials.airtable_key
 
-class OverviewContact < Airrecord::Table
+class OverviewInvoiceSevener < Airrecord::Table
   self.base_key = 'appGm0wPMcSxAI6RH'
-  self.table_name = 'Partners Contacts'
+  self.table_name = 'Invoices - Sevener'
 end
--- app/models/overview_project.rb
+++ app/models/overview_project.rb
@@ -1,6 +1,6 @@
 Airrecord.api_key = Rails.application.credentials.airtable_key
 
-class OverviewContact < Airrecord::Table
+class OverviewProject < Airrecord::Table
   self.base_key = 'appGm0wPMcSxAI6RH'
-  self.table_name = 'Partners Contacts'
+  self.table_name = 'Projects'
 end
