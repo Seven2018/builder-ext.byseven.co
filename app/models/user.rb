02558e5ae6bdff0a9679a15bad05e61d17c5d8ec
Brice Chapuis <bricechapuis>
2021-02-08 10:39:54 +0100

    Bizdev Update Data scheduler

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ApplicationRecord
   end
 
   def self.report
-    UpdateBizdevReportJob.perform
+    UpdateBizdevReportJob.perform_now
   end
 
   def self.from_omniauth(access_token)
