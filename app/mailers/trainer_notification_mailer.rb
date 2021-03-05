f58be18f26ea6b7e42037ba2408fa1532e3cae5e
Brice Chapuis <bricechapuis>
2021-03-05 09:37:49 +0100

    Cleaning : 05/03/21

--- app/mailers/trainer_notification_mailer.rb
+++ app/mailers/trainer_notification_mailer.rb
@@ -5,20 +5,20 @@ class TrainerNotificationMailer < ApplicationMailer
     @training = training
     @sessions = training.sessions.select{|x| x.users.include?(user)}
     @user = user
-    mail(to: "#{user.email}, #{training.owners.first.email}, subject: "#{@training.client_company.name} - #{@training.title} : Récapitulatif intervention")
+    mail(to: "#{user.email}, #{training.owners.first.email}", subject: "#{@training.client_company.name} - #{@training.title} : Récapitulatif intervention")
   end
 
   def edit_trainer_notification(training, user)
     @training = training
     @sessions = training.sessions.select{|x| x.users.include?(user)}
     @user = user
-    mail(to: "#{user.email}, #{training.owners.first.email}, subject: "#{@training.client_company.name} - #{@training.title} : Récapitulatif intervention (Mise à jour du #{Date.today.strftime('%d/%m/%Y')})")
+    mail(to: "#{user.email}, #{training.owners.first.email}", subject: "#{@training.client_company.name} - #{@training.title} : Récapitulatif intervention (Mise à jour du #{Date.today.strftime('%d/%m/%Y')})")
   end
 
   def trainer_session_reminder(session, user)
     @training = session.training
     @session = session
     @user = user
-    mail(to: "#{user.email}, #{session.training.owners.first.email}, subject: "SEVEN : #{session.training.title} - Session du #{session.date.strftime('%d/%m/%Y')}")
+    mail(to: "#{user.email}, #{session.training.owners.first.email}", subject: "SEVEN : #{session.training.title} - Session du #{session.date.strftime('%d/%m/%Y')}")
   end
 end
