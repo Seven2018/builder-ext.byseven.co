2ff3d20086e6a61f1bcf8bd4631d9e9fd734fbe2
Brice Chapuis <bricechapuis>
2021-03-10 17:17:43 +0100

    cleanslate 4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w[ckeditor/config.js, pdf.css]
-Rails.application.config.assets.precompile += %w( application.css )
+Rails.application.config.assets.precompile += %w[ckeditor/config.js, pdf.css, application.css]
+# Rails.application.config.assets.precompile += %w( application.css )
