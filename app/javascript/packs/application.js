bb1201623ab4e577f50c09cb57973039417db3d5
Brice Chapuis <bricechapuis>
2021-03-11 09:15:41 +0100

    cleanslate 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,14 @@ import "../plugins/flatpickr";
 // import {initSortable} from "../plugins/init_sortable.js";
 import {initTree} from "../plugins/tree";
 import 'easy-autocomplete/dist/jquery.easy-autocomplete';
-
+import 'stylesheets/application';
 // initSortable();
 // global.initSortable = initSortable;
 
 
-require("trix")
-require("@rails/actiontext")
+try {
+  require("trix")
+  require("@rails/actiontext")
+}
+catch(err) {
+}
